c2153ab86ea11f3b8705b5c6f680ac378fe5957f
Chethan <86680859+ChethanKumar-K@users.noreply.github.com>
2022-04-18 19:37:54 +0530

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -2,45 +2,45 @@ const form =document.getElementById("form");
 
 function Submit(){
   const email=document.getElementById("email").value;
-  var ar=Array.from(email);
-  const b=['@','b','m','s','c','e','.','a','c','.','i','n'];
-  var x=ar.length;
-  console.log(x);
+  var emailArray=Array.from(email);
+  const lastSequence=['@','b','m','s','c','e','.','a','c','.','i','n'];
+  var emailLength=emailArray.length;
+  console.log(emailLength);
   var j=0;
-  var f=1;
+  var flag=1;
   while(j!=12){
-    if(b[j]===ar[x-12+j]){
+    if(lastSequence[j]===emailArray[x-12+j]){
       j++;
     }
     else{
       alert("You are not allowed to enter");
-      f=0;
+      flag=0;
       break;
     }
   }
-  if(f===1){
+  if(flag===1){
     window.open("index.html");
   }
 }
 function Submit1(){
   const email=document.getElementById("newemail").value;
-  var ar=Array.from(email);
-  const b=['@','b','m','s','c','e','.','a','c','.','i','n'];
-  var x=ar.length;
-  console.log(x);
+  var emailArray=Array.from(email);
+  const lastSequence=['@','b','m','s','c','e','.','a','c','.','i','n'];
+  var emailLength=emailArray.length;
+  console.log(emailLength);
   var j=0;
-  var f=1;
+  var flag=1;
   while(j!=12){
-    if(b[j]===ar[x-12+j]){
+    if(lastSequence[j]===emailArray[x-12+j]){
       j++;
     }
     else{
       alert("Your email id is invalid");
-      f=0;
+      flag=0;
       break;
     }
   }
-  if(f===1){
+  if(flag===1){
     window.open("index.html");
     
   }
